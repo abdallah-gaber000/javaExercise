59c8d4358868a1d8a17a224ad5f36f3e9d982f12
Mahmoud Hefny <hefny4@gmail.com>
2022-07-23 02:24:29 +0200

    This is a message

---
+++ Main.java
@@ -0,0 +1,40 @@
+
+package javaapplication2;
+import java.util.*;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] numbers = {1, 2, 3, 4, 5};
+        for(int i : numbers){
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+        System.out.print("Enter your target: ");
+        int target = s.nextInt();
+        int pointers[] = twoSum(numbers, target);
+        if(pointers[0] == 0 && pointers[1] == 0){
+            System.out.println("Not found");
+            return;
+        }
+        for(int j : pointers){
+            System.out.print(j+" ");
+        }
+        System.out.println("");
+    }
+    
+    private static int[] twoSum(int[] nums, int target) {
+        int[] indicies = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if((nums[i] + nums[j]) == target){
+                    indicies[0] = i;
+                    indicies[1] = j;
+                }
+            }
+        }
+        return indicies;
+    }
+}
